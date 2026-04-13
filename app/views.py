f8f1a47bbb92b49923c1160947b270d74c6bdff5
Coder-GW <98620127+Coder-GW@users.noreply.github.com>
2026-04-12 20:35:43 -0500

    Exercise 2 and 3 commit

--- app/views.py
+++ app/views.py
@@ -4,25 +4,59 @@ Jinja2 Documentation:    https://jinja.palletsprojects.com/
 Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
-
-from app import app
+from app import app, db
 from flask import render_template, request, jsonify, send_file
+from app.models import Movie
+from app.forms import MovieForm
+from werkzeug.utils import secure_filename
+from flask_wtf.csrf import generate_csrf
 import os
-
 
 ###
 # Routing for your application.
 ###
-
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        title       = form.title.data
+        description = form.description.data
+        poster      = form.poster.data
+
+        # Save the file
+        filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Save to database
+        movie = Movie(
+            title=title,
+            description=description,
+            poster=filename
+        )
+        db.session.add(movie)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Movie Successfully added",
+            "title": title,
+            "poster": filename,
+            "description": description
+        })
+
+    return jsonify({"errors": form_errors(form)}), 400
+
+@app.route('/api/v1/csrf-token', methods=['GET'])
+def get_csrf():
+    return jsonify({'csrf_token': generate_csrf()})
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
 def form_errors(form):
@@ -35,7 +69,6 @@ def form_errors(form):
                     error
                 )
             error_messages.append(message)
-
     return error_messages
 
 @app.route('/<file_name>.txt')
@@ -43,7 +76,6 @@ def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
-
 
 @app.after_request
 def add_header(response):
@@ -55,7 +87,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
