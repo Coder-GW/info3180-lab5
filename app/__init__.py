5fb519396a3598d04c2e1616c0f7f9627c1fefcf
Coder-GW <98620127+Coder-GW@users.noreply.github.com>
2026-04-12 21:14:55 -0500

    Exercise 4 commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,22 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
+
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import views, models
+
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect(app)
